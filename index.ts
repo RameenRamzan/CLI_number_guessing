388d92627e449fa6e9b9b9f0129df6120ac56246
RameenRamzan <161635962+RameenRamzan@users.noreply.github.com>
2024-03-26 02:15:35 +0500

    Added Guessing Game

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+  {
+    name: "userGuessNumber",
+    type: "number",
+    message: "Please enter a number between 1-6",
+  },
+]);
+if (answers.userGuessNumber === randomNumber) {
+  console.log("Congratulations! You have guessed the right number");
+} else {
+  console.log("You have guessed the wrong number");
+}
